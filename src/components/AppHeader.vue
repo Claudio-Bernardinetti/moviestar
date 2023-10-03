8f0cb727d381550bfe8011ecdf04775f222a49f5
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-10-03 13:59:36 +0100

    add app header search

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,10 +8,10 @@ export default {
 
 <template>
   <header id="app_header">
-     <div class="top d-flex justify-content-space-around">
+     <div class="top d-flex justify-content-around py-2">
         <div>
             <img class="p-1" src="../../src/assets/img/image (email).svg"  alt="">
-            <span>Superhit Top Movie <strong class="px-1 text-success">**King Star**</strong></span>
+            <span>Superhit Top Movie <strong class="px-1">**King Star**</strong></span>
             <img class="p-1" src="../../src/assets/img/image (f).svg"  alt="">
             <img class="p-1" src="../../src/assets/img/image (t).svg"  alt="">
             <img class="p-1" src="../../src/assets/img/image (in).svg"  alt="">
@@ -23,9 +23,17 @@ export default {
             <span>Login</span>
         </div>
      </div>
-     <div class="search">
-        <img src="../../src/assets/img/cropped-logo.png" alt="">
+     <!-- End Top -->
+     <div class="search d-flex justify-content-around border border-black border-start-0 border-end-0">
+        <div class="p-2">
+          <img src="../../src/assets/img/cropped-logo.png" alt="">
+        </div>
+        <div class="d-flex my-3 py-3">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-success" type="submit">Search</button>
+        </div>
      </div>
+     <!-- End Search -->
      <div class="nav_bar"></div>
      <div class="jumbotron">
      </div>
@@ -36,9 +44,13 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
-.top {
-    color: $Cinema_light;
-    background-color: c;
-}
+#app_header {
+    .top {
+        color: $Cinema_light;
+    }
 
+    strong {
+        color: $Cinema_primary;
+    }
+}
 </style>
